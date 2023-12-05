4d849738fb0f155553a44ccba94ca8b6ad9279ce
tauhashaik <hoosains19@gmail.com>
2023-12-05 17:18:22 +0200

    added search functionality

--- SCRIPT/products.js
+++ SCRIPT/products.js
@@ -26,8 +26,38 @@ main.innerHTML = pokeItems.map(function(item, index){
 displayItems(pokeItems)
 
 let searchBox = document.querySelector('form');
+// Functionailty added to search button. event.preventDefault() to handle submission in a custom manner to update without having to reload the page again.
+searchButt.addEventListener('click', function(event){
+    event.preventDefault();
+    // search bar given variable and made the input value case insensitive.
+    let search = document.getElementById('searchBar').value.toLowerCase();
+    // Filtering through the array of pokeItems for the object names both uppercase and lowercase and the method 'includes' to make sure the search accomodates the lowercase search.
+    let filteredSearch = pokeItems.filter(item => item.name.toLowerCase().includes(search));
 
-searchBox.addEventListener('click', function(){
-    let searchBox=
+    // if the search is more greater than 0 then display the search with the valid inputed character if not greater than 0 and not a valid product the alert message is triggered.
+    if (filteredSearch.length >0){
+        displayItems(filteredSearch);
+    }
+    else{
+        alert("Product not found.")
+    }
+});
+
+// function to display the the the filtered array and corresponding products according to the search parameters by users by maping the the filtered array and displaying the results.
+function displayItems(filteredSearch){
+    main.innerHTML = filteredSearch.map(function(item, index){
+        return `
+        <div id="cards" class="col-lg-3 d-flex justify-content-center">
+            <div class="card h-100" style="width: 18rem;">
+                <img id="imgCard" src="${item.url}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 style="color: yellow; background-color: black;" class="card-title">${item.name}</h5>
+                <p class="card-text" style="font-size: 17px; font-weight: bolder;"><i>${item.description}</i></p>
+                <p class="card-text" style="font-size: 17px;">R${item.price}</p>
+                <button id="adminButt" class="btn btn-primary">Add to Cart</button>
+            </div>
+        </div>
+    </div>
+        `
 })
-
+}
